245a365057ac9bbebe5dfb756a071699d249c218
kim jae hun <zg788990@gmail.com>
2020-10-06 19:05:20 +0900

    revise section position

--- js/main.js
+++ js/main.js
@@ -16,18 +16,49 @@
 
   //1.스크롤의 높이 ,2.
   const sceneInfo = [
-    {
-      //0
+    {//0
       type: 'sticky',
       heightNum: 5,
       scrollHeight: 0,
       objs: {
-        // dom객체 오소들
         container: document.querySelector('#scroll-section-0'),
         messageA: document.querySelector('#scroll-section-0 .main-message.a'),
         messageB: document.querySelector('#scroll-section-0 .main-message.b'),
-        messageC: document.querySelector('#scroll-section-0 .main-message.c'),
-        messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+        pencilLogo: document.querySelector('#scroll-section-0 .pencil-logo'),
+        pencil: document.querySelector('#scroll-section-0 .pencil'),
+        ribbonPath: document.querySelector('.ribbon-path path')
+      },
+      values: {
+        messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+				messageB_opacity_in: [0, 1, { start: 0.4, end: 0.5 }],
+				messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
+				messageA_opacity_out: [1, 0, { start: 0.3, end: 0.4 }],
+				messageB_opacity_out: [1, 0, { start: 0.6, end: 0.7 }],
+				messageA_translateY_out: [0, -20, { start: 0.3, end: 0.4 }],
+				pencilLogo_width_in: [1000, 200, { start: 0.1, end: 0.4 }],
+				pencilLogo_width_out: [200, 50, { start: 0.4, end: 0.8 }],
+				pencilLogo_translateX_in: [-10, -20, { start: 0.2, end: 0.4 }],
+				pencilLogo_translateX_out: [-20, -50, { start: 0.4, end: 0.8 }],
+				pencilLogo_opacity_out: [1, 0, { start: 0.8, end: 0.9 }],
+				pencil_right: [-10, 70, { start: 0.3, end: 0.8 }],
+				pencil_bottom: [-80, 100, { start: 0.3, end: 0.8 }],
+				pencil_rotate: [-120, -200, { start: 0.3, end: 0.8 }],
+				path_dashoffset_in: [1401, 0, { start: 0.2, end: 0.4 }],
+				path_dashoffset_out: [0, -1401, { start: 0.6, end: 0.8 }]
+      }
+    },
+    {
+      //1
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        // dom객체 요소들
+        container: document.querySelector('#scroll-section-1'),
+        messageA: document.querySelector('#scroll-section-1 .main-message.a'),
+        messageB: document.querySelector('#scroll-section-1 .main-message.b'),
+        messageC: document.querySelector('#scroll-section-1 .main-message.c'),
+        messageD: document.querySelector('#scroll-section-1 .main-message.d'),
         canvas: document.querySelector('#video-canvas-0'),
         context: document.querySelector('#video-canvas-0').getContext('2d'),
         videoImages: [],
@@ -58,26 +89,26 @@
       }
     },
     {
-      //1 정적 콘텐츠
+      //2 정적 콘텐츠
       type: 'normal',
       // heightNum: 5,
       scrollHeight: 0,
       objs: {
-        container: document.querySelector('#scroll-section-1')
+        container: document.querySelector('#scroll-section-2')
       }
     },
     {
-      //2
+      //3
       type: 'sticky',
       heightNum: 5,
       scrollHeight: 0,
       objs: {
-        container: document.querySelector('#scroll-section-2'),
-        messageA: document.querySelector('#scroll-section-2 .a'),
-        messageB: document.querySelector('#scroll-section-2 .b'),
-        messageC: document.querySelector('#scroll-section-2 .c'),
-        pinB: document.querySelector('#scroll-section-2 .b .pin'),
-        pinC: document.querySelector('#scroll-section-2 .c .pin'),
+        container: document.querySelector('#scroll-section-3'),
+        messageA: document.querySelector('#scroll-section-3 .a'),
+        messageB: document.querySelector('#scroll-section-3 .b'),
+        messageC: document.querySelector('#scroll-section-3 .c'),
+        pinB: document.querySelector('#scroll-section-3 .b .pin'),
+        pinC: document.querySelector('#scroll-section-3 .c .pin'),
         canvas: document.querySelector('#video-canvas-1'),
         context: document.querySelector('#video-canvas-1').getContext('2d'),
         videoImages: [],
@@ -103,12 +134,12 @@
         pinC_scaleY: [0.5, 1, { start: 0.87, end: 0.92 }],
       }
     },
-    { //3
+    { //4
       type: 'sticky',
       heightNum: 5,
       scrollHeight: 0,
       objs: {
-        container: document.querySelector('#scroll-section-3'),
+        container: document.querySelector('#scroll-section-4'),
         canvasCaption: document.querySelector('.canvas-caption'),
         canvas: document.querySelector('.image-blend-canvas'),
         context: document.querySelector('.image-blend-canvas').getContext('2d'),
@@ -128,61 +159,30 @@
         BlendHeight: [0, 0, { start: 0, end: 0 }], ///애니메이션이 시작되는 순간은 캔버스가 위에 닿을때 
       }
     },
-    {//4
-      type:'sticky',
-      heightnum:3,
-      scrollHeight:0,
-      objs:{
-        container:document.querySelector('#scroll-section-4'),
-        messageA:document.querySelector('#scroll-section-4 .main-message.a'),
-        messageB:document.querySelector('#scroll-section-4 .main-message.b'),
-        pencilLogo:document.querySelector('#scroll-section-4 .pencil-logo'),
-        pencil:document.querySelector('#scroll-section-4 .pencil'),
-        ribbonPath:document.querySelector('.ribbon-path path')
-      },
-      values:{
-        messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
-        messageB_opacity_in: [0, 1, { start: 0.4, end: 0.5 }],
-        messageA_opacity_out: [1, 0, { start: 0.3, end: 0.4 }],
-        messageB_opacity_out: [1, 0, { start: 0.6, end: 0.7 }],
-        messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
-        messageA_translateY_out: [0, -20, { start: 0.3, end: 0.4 }],
-        pencilLogo_width_in: [1000, 200, { start: 0.1, end: 0.4 }],
-				pencilLogo_width_out: [200, 50, { start: 0.4, end: 0.8 }],
-				pencilLogo_translateX_in: [-10, -20, { start: 0.2, end: 0.4 }],
-				pencilLogo_translateX_out: [-20, -50, { start: 0.4, end: 0.8 }],
-        pencilLogo_opacity_out: [1, 0, { start: 0.8, end: 0.9 }],
-        pencil_right: [-10, 70, { start: 0.3, end: 0.8 }],
-				pencil_bottom: [-80, 100, { start: 0.3, end: 0.8 }],
-        pencil_rotate: [-120, -200, { start: 0.3, end: 0.8 }],
-        path_dashoffset_in: [1401, 0, { start: 0.2, end: 0.4 }],
-				path_dashoffset_out: [0, -1401, { start: 0.6, end: 0.8 }]
-      }
-    }
   ];
 
   // canvas의 이미지를 세팅해주는 함수
   function setCanvasImages() {
     //첫번째 section의 이미지 처리
     let imgElem;
-    for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
+    for (let i = 0; i < sceneInfo[1].values.videoImageCount; i++) {
       imgElem = new Image();//이미지 객체를 하나 만듦
       // imgElem = document.createElement('img')
       imgElem.src = `./video/001/IMG_${6726 + i}.JPG`;
-      sceneInfo[0].objs.videoImages.push(imgElem);
+      sceneInfo[1].objs.videoImages.push(imgElem);
     }
 
     let imgElem2;
-    for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
+    for (let i = 0; i < sceneInfo[3].values.videoImageCount; i++) {
       imgElem2 = new Image();
       imgElem2.src = `./video/002/IMG_${7027 + i}.JPG`;
-      sceneInfo[2].objs.videoImages.push(imgElem2);
+      sceneInfo[3].objs.videoImages.push(imgElem2);
     }
     let imgElem3;
-    for (let i = 0; i < sceneInfo[3].objs.imagePath.length; i++) {
+    for (let i = 0; i < sceneInfo[4].objs.imagePath.length; i++) {
       imgElem3 = new Image();
-      imgElem3.src = sceneInfo[3].objs.imagePath[i];
-      sceneInfo[3].objs.images.push(imgElem3)
+      imgElem3.src = sceneInfo[4].objs.imagePath[i];
+      sceneInfo[4].objs.images.push(imgElem3)
     }
     // console.log(sceneInfo[3].objs.images)
     // console.log(sceneInfo[0].objs.videoImages)
@@ -227,8 +227,8 @@
     document.body.setAttribute('id', `show-scene-${currentScene}`);
 
     const heightRatio = window.innerHeight / 1080;
-    sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%,-50%,0) scale(${heightRatio})`;
-    sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%,-50%,0) scale(${heightRatio})`;
+    sceneInfo[1].objs.canvas.style.transform = `translate3d(-50%,-50%,0) scale(${heightRatio})`;
+    sceneInfo[3].objs.canvas.style.transform = `translate3d(-50%,-50%,0) scale(${heightRatio})`;
 
   }
   //currentYoffset은 현재의 씬이 얼마나 스크롤 되었는지?
@@ -276,7 +276,48 @@
     // console.log('cur',currentScene)
     switch (currentScene) {
       case 0:
-        // console.log('0 play');
+        if (scrollRatio <= 0.25) {
+          //in 
+          objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currrentYOffset);
+          objs.messageA.style.transform = `translate3d(0,${calcValues(values.messageA_translateY_in, currrentYOffset)}%,0)`;
+        } else {
+          //out
+          objs.messageA.style.opacity = calcValues(values.messageA_opacity_out, currrentYOffset);
+          objs.messageA.style.transform = `translate3d(0,${calcValues(values.messageA_translateY_out, currrentYOffset)}%,0)`;
+        }
+        if (scrollRatio <= 0.55) {
+          //in
+          objs.messageB.style.opacity = calcValues(values.messageB_opacity_in, currrentYOffset);
+        } else {
+          //out
+          objs.messageB.style.opacity = calcValues(values.messageB_opacity_out, currrentYOffset);
+        }
+        //크기가 커져도 깨지지안흔ㄴ svg의 장점을 살리기 위해 tranform scale대신 width조정
+        if (scrollRatio <= 0.4) {
+          objs.pencilLogo.style.width = `${calcValues(values.pencilLogo_width_in, currrentYOffset)}vw`;
+          objs.pencilLogo.style.transform = `translate3d(${calcValues(values.pencilLogo_translateX_in, currrentYOffset)}%,-50%)`;
+
+        } else {
+          objs.pencilLogo.style.width = `${calcValues(values.pencilLogo_width_out, currrentYOffset)}vw`;
+          objs.pencilLogo.style.transform = `translate3d(${calcValues(values.pencilLogo_translateX_out, currrentYOffset)}%,-50%)`;
+
+        }
+        //빨간 리본 패스(줄 긋기)
+        if (scrollRatio <= 0.5) {
+					objs.ribbonPath.style.strokeDashoffset = calcValues(values.path_dashoffset_in,currrentYOffset);
+				} else {
+					objs.ribbonPath.style.strokeDashoffset = calcValues(values.path_dashoffset_out,currrentYOffset);
+				}
+
+        objs.pencilLogo.style.opacity = calcValues(values.pencilLogo_opacity_out, currrentYOffset);
+				objs.pencil.style.right = `${calcValues(values.pencil_right, currrentYOffset)}%`;
+				objs.pencil.style.bottom = `${calcValues(values.pencil_bottom, currrentYOffset)}%`;
+				objs.pencil.style.transform = `rotate(${calcValues(values.pencil_rotate, currrentYOffset)}deg)`;
+
+        break;
+
+      case 1:
+        // console.log('1 play');
         //시쿼스에 따라 이미지가 바뀜
         // 인덱스로 사용
         // let sequence = Math.round(calcValues(values.imageSquence, currrentYOffset));
@@ -327,7 +368,7 @@
         }
         break;
 
-      case 2:
+      case 3:
         //시쿼스에 따라 이미지가 바뀜
         // let sequence2 = Math.round(calcValues(values.imageSquence, currrentYOffset));
         // objs.context.drawImage(objs.videoImages[sequence2], 0, 0);
@@ -423,7 +464,7 @@
         }
         break;
 
-      case 3:
+      case 4:
         //가로 세로 모두 꽈 차게 하기 위해 세팅(계산 필요)
         const widthRatio = window.innerWidth / objs.canvas.width;
         const heightRatio = window.innerHeight / objs.canvas.height;
@@ -562,47 +603,6 @@
           }
         }
         break;
-      case 4:
-      if(scrollHeight <= 0.25){
-        //in 
-        objs.messageA.style.opacity = calcValues(values.messageA_opacity_in,currrentYOffset);
-        objs.messageA.style.transform = `translate3d(0,${calcValues(values.messageA_translateY_in,currrentYOffset)}%,0)`;
-      }else{
-        //out
-        objs.messageA.style.opacity  = calcValues(values.messageA_opacity_out,currrentYOffset);
-        objs.messageA.style.transform = `translate3d(0,${calcValues(values.messageA_translateY_out,currrentYOffset)}%,0)`;
-      }
-      if(scrollHeight <= 0.55){
-        //in
-        objs.messageB.style.opacity = calcValues(values.messageB_opacity_in,currrentYOffset);
-      }else {
-        //out
-        obj.messageB.style.opacity = calcValues(values.messageB_opacity_out,currrentYOffset);
-      }
-      //크기가 커져도 깨지지안흔ㄴ svg의 장점을 살리기 위해 tranform scale대신 width조정
-      if(scrollRATIO <= 0.4){
-        objs.pencilLogo.style.width = `${calcValues(valuse.pencilLogo_width_in,currrentYOffset)}vw`;
-        objs.pencilLogo.style.transform = `translate3d(${calcValues(values.pencilLogo_translateX_in,currrentYOffset)}%,-50%)`;
-
-      }else{
-        objs.pencilLogo.style.width = `${calcValues(values.pencilLogo_width_out,currrentYOffset)}vw`;
-        objs.pencilLogo.style.tranform = `translate3d(${calcValues(values.pencilLogo_translateX_out,currrentYOffset)}%,-50%)`;
-
-      }
-      //빨간 리본 패스(줄 긋기)
-      if(scrollRatio <= 0.5){
-        //in
-        objs.ribbonPath.style.strokeDashoffset = calcValues(valuse.path_dashoffset_in,currrentYOffset);
-      }else{
-        //off
-        objs.ribbonPath.style.strokeDashoffset = calcValues(values.path_dashoffset_out,currrentYOffset);
-      }
-      objs.pencilLogo.style.opacity = calcValues(values.pencilLogo_opacity_out,currrentYOffset);
-      objs.pencil.style.right = `${calcValues(values.pencil_right,currrentYOffset)}%`;
-      objs.pencil.style.bottom = `${calcValues(values.pencil_bottom,currrentYOffset)}%`;
-      objs.pencil.style.transform = `rotate(${calcValues(values.pencil_rotate,currrentYOffset)}deg)`;
-
-      break;
     }
   }
 
@@ -615,7 +615,7 @@
       prevScrollHeight += sceneInfo[i].scrollHeight;
 
     }
-    if(delayedYOffset < prevScrollHeight +sceneInfo[currentScene].scrollHeight){
+    if (delayedYOffset < prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       document.body.classList.remove('sticky-effect-end');
     }
 
@@ -623,15 +623,16 @@
     if (delayedYOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       enterNewScene = true;
       //현재의 씬이 마지막 씬일때
-      console.log('currentScene',currentScene);
-      console.log('sceneInfo.length',sceneInfo.length);
-      if(currentScene === sceneInfo.length - 1){
+      console.log('currentScene', currentScene);
+      console.log('sceneInfo.length', sceneInfo.length);
+
+      if (currentScene === sceneInfo.length - 1) {
         document.body.classList.add('sticky-effect-end');
       }
-      if(currentScene < sceneInfo.length - 1){
+      if (currentScene < sceneInfo.length - 1) {
         currentScene++;
       }
-      
+
       document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
     //값을 Yoffset이 아닌 감속을 적용한 변수인 deleayedYOffset을 적용한다.
@@ -691,7 +692,7 @@
     //트랜지션이 끝이 나면 loading 요소를 제거해 준다.
     setLayout();
     //laod될때 첫 화면에 가장 첫번째 사진을 보이게 함
-    sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
+    sceneInfo[1].objs.context.drawImage(sceneInfo[1].objs.videoImages[0], 0, 0);
 
     let tempYOffset = yOffset;
     let tempScrollCount = 0; //5px씩 몇번했는지 count할 변수
@@ -724,11 +725,10 @@
     window.addEventListener('resize', () => {
       // landscape 모드에서 대부분의 디바이스의 너비는 900이 넘어간다.
       if (window.innerWidth > 900) {
-
-        window.location.reload();
-        // setLayout();
+        // window.location.reload();
+        setLayout();
         // //resize 되었을때 sceneInfo[3].values.restStartY = 0 으로 초기화한다.
-        // sceneInfo[3].values.rectStartY = 0;
+        sceneInfo[3].values.rectStartY = 0;
       }
     });
 
@@ -736,10 +736,10 @@
     //canvas의 크기를 셋팅하는 문제가 있다.
     window.addEventListener('orientationchange', () => {
       scrollTo(0, 0);
-      // setTimeout(setLayout, 500);
-      setTimeout(() => {
-        window.location.reload()
-      }, 500);
+      setTimeout(setLayout, 500);
+      // setTimeout(() => {
+      //   window.location.reload()
+      // }, 500);
     });
 
     //loading에 transitionend라는 이벤트를 바인딩 시켜서 이벤트 객체를 통해 현재 자신의 요소를 removeChild시킨다.
@@ -748,12 +748,6 @@
     })
 
   });
-
-
-
   setCanvasImages();
-
-
-
 })();
 
